46f8efb0fd730744bc26a301da55ac90a4a209b2
benr <b.ricketts@sron.nl>
2023-03-07 13:39:50 +0100

    Removed data generation and replaced with generating data during training. Also added basic train and test functions

--- generator.py
+++ generator.py
@@ -2,10 +2,16 @@
 This program deals with generating rtdist models with given parameters. For use
 for generating sampels for training the emulator for rtdist.
 """
-
-from reltrans import _models
 import numpy as np
-import torch
+from reltrans import _models
+from torch import data
+
+class IterDataset(data.IterableDataset):
+    def __init__(self, generator):
+        self.generator = generator
+
+    def __iter__(self):
+        return self.generator()
 
 def rtdist_flux(pars,egrid):
     """
@@ -49,42 +55,3 @@ def rtdist_lags(pars,egrid):
     dE = np.diff(egrid)
     output = y[:-1]/dE
     return output
-
-def parameter_gen():
-    height = np.linspace(1.3,1e4,int((1e4 - 1.3)/0.1))
-    spin = np.linspace(0,0.998,int(0.998/0.001))
-    inc = np.linspace(1,80,int(79/1e-2))
-    rin = np.linspace(-400,-1,int(399/1e-2))
-    rout = np.linspace(400,1e5,int((1e5 - 400)))
-    z = np.linspace(0,10,int(10/0.001))
-    Gamma = np.linspace(1.4,3.4,int(2/0.01))
-    Dkpc = np.logspace(np.log10(0.2),10,num=10000)
-    Afe = np.linspace(0.5,10,int(9.5/1e-2))
-    logNe = np.linspace(15,20,int(5/0.01))
-    nH = np.logspace(-10,6,num=10000)
-    boost = np.linspace(1e-2,10,int(10/1e-2))
-    mass = np.logspace(0,11,num=10000)
-    honr = np.linspace(0,0.176,int(0.176/1e-4))
-    b1 = np.linspace(0,2,int(2/1e-2))
-    b2 = np.linspace(-4,4,int(8/1e-2))
-    fmin = np.logspace(-10,6,num=10000)
-    fmax = np.logspace(-10,6,num=10000)
-    phiAB = np.linspace(-6.283,6.283,int((6.283)*2/0.001))
-    g = np.linspace(0,0.5,int(0.5/0.01))
-    Anorm = np.logspace(-12,10,num=10000)
-    grid = np.asarray([height,spin,inc,rin,rout,z,Gamma,Dkpc,Afe,logNe,nH,boost,
-                       mass,honr,b1,b2,fmin,fmax,phiAB,g,Anorm],dtype=object)
-    grid = np.meshgrid(grid)
-    for dimension in grid:
-        print(dimension)
-    return grid
-
-
-def lag_gen(egrid):
-    pars = parameter_gen()
-    data = []
-    for i,par in enumerate(pars):
-        return
-    return data
-
-parameter_gen()
--- main.py
+++ main.py
@@ -11,20 +11,54 @@ from torchvision.transforms import ToTensor
 from sherpa.astro.ui import unpack_arf,unpack_rmf
 import os
 
+def train(model,optimizer,loss_fn,true_func,par_gen):
+    model.train()
+    batches = 1000
+    for batch in range(batches):
+        pars = par_gen()
+        truth = true_func(pars)
+        pred = model(pars)
+        loss = loss_fn(pred,truth)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{batches:>5d}]")
+    
+    return model, optimizer
+
+def test(model,loss_fn,true_func,par_gen):
+    model.eval()
+    test_loss = 0
+    batches = 1000
+    with torch.no_grad():
+        for batch in range(batches):
+            pars = par_gen()
+            truth = true_func(pars)
+            pred = model(pars)
+            test_loss += loss_fn(pred, truth).item()
+    test_loss /= batches
+    print(f"Avg loss: {test_loss:>8f}")
+
 wrk_dir = os.getcwd()
 rmf_name = wrk_dir+"/ResponseFiles/PN.rmf"
-
 rmf = unpack_rmf(rmf_name)
-
 egrid = rmf.e_min
 
-pars = [6,0.9,57,-1,2e4,0.024917,2.45,1e5,1,17,50.,5,1,3e6,0.02,0,0,4e-5,20e-5,0.95,0,
-        -0.8,0.3,2.2e-4,260000.,400,1,1.]
-
-training_data = generator.rtdist_flux(pars, egrid)
-
-batch_size = 64
-
-train_dataloader = DataLoader(training_data, batch_size = batch_size)
 model = network.NeuralNetwork()
+optimizer = 0
+max_iters = 10000
+i = 0
 
+print("Beginning training")
+while i < max_iters:
+    print(f"Epoch {i+1} \n -----------------------")
+    train(model,optimizer,nn.MSEloss,generator.rtdist_lags,generator.par_gen)
+    test(model,nn.MSEloss,generator.rtdist_lags,generator.par_gen)
+    
+print("Completed training")
+torch.save(model.state_dict(), "model.pth")
+print("Saved PyTorch Model State to model.pth")
