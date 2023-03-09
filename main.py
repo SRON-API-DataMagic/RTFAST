f4b7b724205a51c65e76f572d16e1b8b34760457
benr <b.ricketts@sron.nl>
2023-03-09 15:27:21 +0100

    Testing

--- main.py
+++ main.py
@@ -81,6 +81,7 @@ def train(dataloader,model,optimizer,loss_fn):
     batch_size = 12
     for batch, (D,P) in enumerate(dataloader):
         D[D==0] = -50
+        print(np.any(D==0))
         D = torch.log10(D)
         D = D.double()
         pred = model(P)
