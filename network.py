d40eb274e2a1cb8c2358a39f9d4449c26022dc37
benr <b.ricketts@sron.nl>
2023-03-09 16:47:47 +0100

    Fix

--- network.py
+++ network.py
@@ -3,8 +3,6 @@ This program comprimises the neural network structure used as a base for the
 rtdist emulator.
 """
 from torch import nn
-import torch
-
 
 class NeuralNetwork(nn.Module):
     
@@ -12,14 +10,12 @@ class NeuralNetwork(nn.Module):
         super().__init__()
         self.flatten = nn.Flatten()
         self.LinearStack = nn.Sequential(
-            nn.Linear(26,data_len),
-            nn.ReLU()
+            nn.Linear(26,data_len)
             ) 
         self.double()
     
     def forward(self,pars):
         pars = self.flatten(pars)
         result = self.LinearStack(pars)
-        result = torch.mul(result,-1)
         return result
 
