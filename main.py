85312de035ebd261291ed9c8dbce457b38077e3f
benr <b.ricketts@sron.nl>
2023-03-09 15:24:22 +0100

    Train in log space

--- main.py
+++ main.py
@@ -80,6 +80,7 @@ def train(dataloader,model,optimizer,loss_fn):
     size = len(dataloader.dataset)
     batch_size = 12
     for batch, (D,P) in enumerate(dataloader):
+        D[D==0] = -50
         D = torch.log10(D)
         D = D.double()
         pred = model(P)
@@ -99,6 +100,7 @@ def test(dataloader,model,loss_fn):
     batches = len(dataloader)
     with torch.no_grad():
         for batch, (D,P) in enumerate(dataloader):
+            D[D==0] = -50
             D = torch.log10(D)
             pred = model(P)
             test_loss += loss_fn(pred, D).item()
