64b1326798a77db47cc578d112f63db35839c9f9
benr <b.ricketts@sron.nl>
2023-03-09 16:21:51 +0100

    Reduce amount of prints

--- main.py
+++ main.py
@@ -90,8 +90,9 @@ def train(dataloader,model,optimizer,loss_fn):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        loss, current = loss.item(), (batch*batch_size + 1)
-        print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch*batch_size + 1)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
         loss_arr.append(loss)
         batch += 1
     loss_arr = np.asarray(loss_arr)
