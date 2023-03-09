bb3b53cfbf25bde1f72df996ac202adc35ec70f3
benr <b.ricketts@sron.nl>
2023-03-09 15:39:19 +0100

    Testing

--- main.py
+++ main.py
@@ -80,7 +80,7 @@ def train(dataloader,model,optimizer,loss_fn):
     size = len(dataloader.dataset)
     batch_size = 12
     for batch, (D,P) in enumerate(dataloader):
-        D[D==0] = 1e-50
+        D[D<1e-10] = 1e-10
         D = torch.log10(D)
         D = D.double()
         pred = model(P)
@@ -100,7 +100,7 @@ def test(dataloader,model,loss_fn):
     batches = len(dataloader)
     with torch.no_grad():
         for batch, (D,P) in enumerate(dataloader):
-            D[D==0] = -50
+            D[D<1e-10] = 1e-10
             D = torch.log10(D)
             pred = model(P)
             test_loss += loss_fn(pred, D).item()
