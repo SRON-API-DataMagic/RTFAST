dee57bf29ecd926f177f4c759156badb7869a35e
benr <b.ricketts@sron.nl>
2023-03-09 10:40:39 +0100

    Added in necessary environmental variables for automated model creation

--- main.py
+++ main.py
@@ -78,6 +78,22 @@ def test(model,loss_fn,true_func,par_gen):
     print(f"Avg loss: {test_loss:>8f}")
 
 wrk_dir = os.getcwd()
+
+#set envionmental variables required in xspec with simrtdist
+environ_vars = {"REV_VERB":"0","MU_ZONES":"1","ION_ZONES":"1","A_DENSITY":"1",
+                "EMIN_REF":"0.5","EMAX_REF":"10","EMIN_REF2":"0.5",
+                "EMAX_REF2":"10", "SEED_SIM":"-2851043",
+                "RMF_SET":wrk_dir+"/ResponseFiles/PN.rmf",
+                "ARF_SET":wrk_dir+"/ResponseFiles/PN.arf",
+                "BKG_SET":wrk_dir+"/ResponseFiles/PNbackground_spectrum.fits",
+                "BACKSCL":"1.0"}
+
+for key in environ_vars:
+    os.environ[key] = environ_vars[key]
+
+print("Environmental variables successfully set")
+
+
 rmf_name = wrk_dir+"/ResponseFiles/PN.rmf"
 rmf = unpack_rmf(rmf_name)
 egrid = rmf.e_min
