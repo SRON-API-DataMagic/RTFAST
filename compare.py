914033f7062bc6b40b4e33b32010b651522131e8
benr <b.ricketts@sron.nl>
2023-03-13 14:42:27 +0100

    Testing

--- compare.py
+++ compare.py
@@ -72,6 +72,7 @@ for batch, (D,P) in enumerate(testing_dataloader):
     axs[0].plot(egrid,da,c="r",label="Truth",lw=1.)
     axs[0].legend()
     axs[0].text(0.75,0.75,f"Spin, Mass: {P[0][0].item(),P[0][1].item()}")
+    print(f"Spin, Mass: {P[0][0].item(),P[0][1].item()}")
     axs[0].set_ylabel("Flux")
     axs[1].scatter(egrid,(da-pred)/da,s=0.5)
     axs[1].set_ylabel("Residuals")
