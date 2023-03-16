29e703d5f2cc09b97e274de8fe3ba6fe85bddc06
benr <b.ricketts@sron.nl>
2023-03-16 12:51:38 +0100

    Correctly load text not pickle

--- compare.py
+++ compare.py
@@ -48,8 +48,8 @@ class CustomData(Dataset):
     
 wrk_dir = os.getcwd()
 
-tr_loss_arr = np.load("tr_loss.txt")
-te_loss_arr = np.load("te_loss.txt")
+tr_loss_arr = np.loadtxt("tr_loss.txt")
+te_loss_arr = np.loadtxt("te_loss.txt")
 
 plt.plot(tr_loss_arr,label="training loss")
 plt.plot(te_loss_arr,label="testing loss")  
