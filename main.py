4b314c391dac281148b7903d84e8b6369a88291c
benr <b.ricketts@sron.nl>
2023-03-16 15:29:59 +0100

    Testing

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ class CustomData(Dataset):
         """
         D = self.data
         D_np = D.numpy()
+        print(D_np)
         D_np = self.scale(D_np)
         D = torch.from_numpy(D_np)
         D = D.double()
