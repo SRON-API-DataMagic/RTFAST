a127e55f7ef973f52f93cc4c819d5286c4ccc010
benr <b.ricketts@sron.nl>
2023-03-29 12:29:17 +0200

    Using trimmed parameter generator, not full

--- main.py
+++ main.py
@@ -260,7 +260,7 @@ def main():
     
     active_loops = 20
     epochs = 100
-    range_all = np.asarray(generator.lhs_range_gen())
+    range_all = np.asarray(generator.lhs_trimmed_gen())
     n_samples = 500
     n_samples_large = 75000 # number of parameter sets to draw 
     
