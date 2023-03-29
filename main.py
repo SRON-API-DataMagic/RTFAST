f7102a3e80d6514e77621f75de6ad5eb6a2b9d27
benr <b.ricketts@sron.nl>
2023-03-29 14:16:30 +0200

    Dramatically reduced size of draws in view of memory

--- main.py
+++ main.py
@@ -262,7 +262,7 @@ def main():
     epochs = 100
     range_all = np.asarray(generator.lhs_trimmed_gen())
     n_samples = 500
-    n_samples_large = 75000 # number of parameter sets to draw 
+    n_samples_large = 10000 # number of parameter sets to draw 
     
     #pre generate Latin Hypercube samples.
     sampler = scipy.stats.qmc.LatinHypercube(d=len(range_all))
