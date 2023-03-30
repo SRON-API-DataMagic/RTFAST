3474a3baebfe5d9dee3ca376b55c53542b962c62
benr <b.ricketts@sron.nl>
2023-03-30 14:34:08 +0200

    Change to MinMaxScaler

--- main.py
+++ main.py
@@ -294,7 +294,7 @@ def main():
         data_init = data
         theta_init = pars
     
-    scaler = StandardScaler()
+    scaler = MinMaxScaler()
     #create initial dataset object to create scaler (and then delete object)
     data_init_dataset = CustomData(theta_init, data_init, scaler)
     del data_init_dataset
