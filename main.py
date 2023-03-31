b98a5e39d7336f4d416b9f6aa0536ddcf7620938
benr <b.ricketts@sron.nl>
2023-03-31 16:52:30 +0200

    Dramatically reduce number of dropout calculations

--- main.py
+++ main.py
@@ -329,13 +329,7 @@ def main():
         
         print("computing neural network predictions with dropout for each theta")
         # compute 100 neural network predictions with dropout
-        pred_query_all = np.zeros((500,len(egrid)))
-        for i in range(500):
-            if i % 10 == 0:
-                print(f"Computing theta {i+1}")
-            pred_query = model(torch.DoubleTensor(theta_query_large))
-            pred_query_all[i] = pred_query.detach().numpy()
-            print(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+        pred_query_all = model(torch.DoubleTensor(theta_query_large)).detach().numpy()
         
         print("Successfully finished generating thetas")
         
