a375a011e546cc0750190aa24c55609136498372
benr <b.ricketts@sron.nl>
2023-04-19 12:03:35 +0200

    Checking sizes of querys

--- main.py
+++ main.py
@@ -334,6 +334,7 @@ def main():
         # sort these thetas from largest uncertainty (as measured by 
         # relative variance) to smallest
         query_idx = np.asarray(query_idx)
+        print(query_idx.shape)
         query_idx = np.argsort(mean_var_query)[::-1]
         
         print("Generating data for these samples")
