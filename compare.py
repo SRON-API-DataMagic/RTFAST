e68bd4b6d1eb02401139d7b7b21a37d740824724
benr <b.ricketts@sron.nl>
2023-05-02 14:30:02 +0200

    Minor plotting changes

--- compare.py
+++ compare.py
@@ -430,11 +430,11 @@ def main():
     print("Plotting loss by sample size")
     
     plt.fill_between(grid_sample_nums, grid_loss+grid_loss_std, 
-                     grid_loss-grid_loss_std, alpha = 0.5)
-    plt.plot(grid_sample_nums,grid_loss,label="Grid")
+                     grid_loss-grid_loss_std, alpha = 0.5,color = "b")
+    plt.plot(grid_sample_nums,grid_loss,label="Grid",color = "b")
     plt.fill_between(active_sample_nums, active_loss+active_loss_std, 
-                     active_loss-active_loss_std, alpha = 0.5)
-    plt.plot(active_sample_nums,active_loss,label="Active learning")
+                     active_loss-active_loss_std, alpha = 0.5,color = "orange")
+    plt.plot(active_sample_nums,active_loss,label="Active learning",color = "orange")
     plt.axhline(y=1, ls = "--",label="1% error")
     plt.yscale("log")
     plt.xlabel("Number of samples used in training")
