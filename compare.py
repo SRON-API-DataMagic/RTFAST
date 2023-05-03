ac3d5f0fc3f570f850defbae10d16d96c4c89dee
benr <b.ricketts@sron.nl>
2023-05-03 09:39:49 +0200

    Enforce model evaluation and plot heatmaps

--- compare.py
+++ compare.py
@@ -386,17 +386,16 @@ def active_v_grid(wrk_dir,egrid,testing_dataloader,active_scaler,grid_scaler):
     for (model_loc,gr) in zip(active_model_names,active_name):
         print(gr)
         model = model_load(model_loc, egrid)
+        model.eval()
         loss,loss_std = calculate_loss(testing_dataloader, model, active_scaler)
         active_loss.append(loss)
         active_loss_std.append(loss_std)
-        """
         (mass_res,mass_res_flat,spin_res,spin_res_flat,mass_tick, 
                           mass_ticklabel,spin_ticklabel,
                           spin_tick) = residual_computation(testing_dataloader, model, grid_scaler)
         heatmap_plots(mass_res,mass_res_flat,spin_res,spin_res_flat,mass_tick, 
                           mass_ticklabel,spin_ticklabel,
                           spin_tick,gr)
-        """
     
     active_loss = np.asarray(active_loss)
     active_loss_std = np.asarray(active_loss_std)
@@ -405,17 +404,16 @@ def active_v_grid(wrk_dir,egrid,testing_dataloader,active_scaler,grid_scaler):
     for (model_loc,gr) in zip(grid_model_names,grid_name):
         print(gr)
         model = model_load(model_loc, egrid)
+        model.eval()
         loss,loss_std = calculate_loss(testing_dataloader, model, grid_scaler)
         grid_loss.append(loss)
         grid_loss_std.append(loss_std)
-        """
         (mass_res,mass_res_flat,spin_res,spin_res_flat,mass_tick, 
                           mass_ticklabel,spin_ticklabel,
                           spin_tick) = residual_computation(testing_dataloader, model, grid_scaler)
         heatmap_plots(mass_res,mass_res_flat,spin_res,spin_res_flat,mass_tick, 
                           mass_ticklabel,spin_ticklabel,
                           spin_tick,gr)
-        """
     
     grid_loss = np.asarray(grid_loss)
     grid_loss_std = np.asarray(grid_loss_std)
