a9e45fb64b48428b4701a17aff06e07c8bc71f09
benr <b.ricketts@sron.nl>
2023-05-15 17:16:27 +0200

    Testing

--- compare.py
+++ compare.py
@@ -350,12 +350,11 @@ def calculate_loss(testing_dataloader,model,scaler):
     residuals = []
     for batch, (D,P,M) in enumerate(testing_dataloader):
         values,counts = np.unique(M,return_counts=True)
-        print(values)
-        print(counts)
         pred = model(P).detach().numpy()
         pred = 10**(inverse(scaler,pred))
         resid = (D-pred)/D
         resid = resid*M
+        print(resid)
         residuals.append(np.absolute(np.asarray(resid)))
     
     residuals = np.asarray(residuals)
