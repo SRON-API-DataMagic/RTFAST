6ecf3398728078982e43a0304d730de008cf63b0
benr <b.ricketts@sron.nl>
2023-05-16 09:38:57 +0200

    Fix

--- compare.py
+++ compare.py
@@ -310,6 +310,7 @@ def residual_computation(testing_dataloader,model,scaler):
 
 def model_samples(testing_dataloader,scaler,model,egrid,gr):
     for batch, (D,P,M) in enumerate(testing_dataloader):
+        M = np.squeeze(M)
         #retrieve relevant data and parameters
         spin, mass = P[0][0].item(),10**P[0][1].item()
         D[M==0] = 1e-38
