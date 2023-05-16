c7e93d5dc9be29d4bfa45336b97509c38f99a1fd
benr <b.ricketts@sron.nl>
2023-05-16 09:23:08 +0200

    Bug fix

--- compare.py
+++ compare.py
@@ -317,8 +317,8 @@ def model_samples(testing_dataloader,scaler,model,egrid,gr):
         
         #generate neural network prediction and rescale to linear space
         pred = model(P).detach().numpy()
-        pred = 10**np.squeeze(inverse(scaler,pred))
         pred[M==0] = 1e-38
+        pred = 10**np.squeeze(inverse(scaler,pred))
         
         fname = f"{batch}_res"
         title = "Standard output"
@@ -326,8 +326,9 @@ def model_samples(testing_dataloader,scaler,model,egrid,gr):
         residual_plots(egrid, pred, da, spin, mass, fname, title, gr, norm = True)
         
         pred = model(P).detach().numpy()
-        pred = np.squeeze(inverse(scaler,pred))
         pred[M==0] = np.log10(1e-38)
+        pred = np.squeeze(inverse(scaler,pred))
+        
         
         da_log = np.log10(da)
         
@@ -337,8 +338,8 @@ def model_samples(testing_dataloader,scaler,model,egrid,gr):
         residual_plots(egrid, pred, da_log, spin, mass, fname, title, gr)
         
         pred = model(P).detach().numpy()
-        pred = np.squeeze(pred)
         pred[M==0] = scaler.transform(np.log10(1e-38))
+        pred = np.squeeze(pred)
         
         fname = f"{batch}_res_scal"
         title = "Neural network normalised output"
