c7fac1472e095f9fb096b343980af8d5730a06df
benr <b.ricketts@sron.nl>
2023-05-17 14:27:27 +0200

    More fixes

--- compare.py
+++ compare.py
@@ -96,8 +96,13 @@ def sample_dist_plots(pars):
 def flat_heatmap(df,index,ticks,ticklabels,fname):
     cmap_flat = sns.color_palette("hls", 2)
     
+    residuals = []
+    for row in df.iterrows():
+        residuals.append(row["Residuals"].flat)
+    residuals = np.asarray(residuals)
+    
     fig = plt.figure(figsize=(10,10))
-    ax = sns.heatmap(df["Residuals"].flat,cmap=cmap_flat,cbar_kws = {})
+    ax = sns.heatmap(residuals,cmap=cmap_flat,cbar_kws = {})
     ax.set_yticks(ticks,labels=ticklabels)
     ax.set_xticks(np.arange(0,4096,4096/4), labels=np.arange(0,20,5))
     ax.set_xlabel("Energy in keV")
@@ -112,8 +117,13 @@ def flat_heatmap(df,index,ticks,ticklabels,fname):
     return
 
 def continuous_heatmap(df,index,ticks,ticklabels,fname):
+    residuals = []
+    for row in df.iterrows():
+        residuals.append(row["Residuals"].flat)
+    residuals = np.asarray(residuals)
+    
     fig = plt.figure(figsize=(10,10))
-    ax = sns.heatmap(df["Residuals"].data,cmap="vlag", vmin = 0, vmax = 0.05, center = 0.01)
+    ax = sns.heatmap(residuals,cmap="vlag", vmin = 0, vmax = 0.05, center = 0.01)
     ax.set_yticks(ticks,labels=ticklabels)
     ax.set_xticks(np.arange(0,4096,4096/4), labels=np.arange(0,20,5))
     ax.set_xlabel("Energy in keV")
