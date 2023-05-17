c7b0e289c633a1ff5d66e919ac0b2ca9ed3c5623
benr <b.ricketts@sron.nl>
2023-05-17 17:16:25 +0200

    Making better heatmaps

--- compare.py
+++ compare.py
@@ -94,7 +94,7 @@ def sample_dist_plots(pars):
     plt.close()
     
 def flat_heatmap(df,index,ticks,ticklabels,fname):
-    cmap_flat = sns.color_palette("hls", 2)
+    cmap_flat = sns.color_palette("hls_r", 2)
     
     residuals= []
     for indice,row in df.iterrows():
