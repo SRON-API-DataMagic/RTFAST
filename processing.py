461ede79556f00dbd639e0f798ee19711b818fc3
benr <b.ricketts@sron.nl>
2023-05-18 15:38:07 +0200

    Don't save parameters and locations together

--- processing.py
+++ processing.py
@@ -3,20 +3,16 @@ This module generally deals with data processing for plotting and retrieval
 """
 
 import numpy as np
-import os, glob
 
 def breakup(dataset_loc,pars_loc,destination):
     dataset = np.loadtxt(dataset_loc)
-    pars = np.loadtxt(pars_loc)
     
     locations = []
-    for i,(spectra,pars) in enumerate(zip(dataset,pars)):
+    for i,spectra in enumerate(dataset):
         loc = f"data/spectra/spectra_{i}.txt"
         print(loc)
-        print(spectra)
         np.savetxt(loc,spectra)
-        row = pars.tolist()+[loc]
-        locations.append(row)
+        locations.append(loc)
     
     locations = np.asarray(locations)
     np.savetxt("data/spectra_loc.txt",locations)
