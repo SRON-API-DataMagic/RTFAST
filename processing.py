81487ead817ea9920d2c5c11a4433ab0e3d27099
benr <b.ricketts@sron.nl>
2023-05-18 16:08:17 +0200

    Specifying that location array should be saved in string format

--- processing.py
+++ processing.py
@@ -15,7 +15,7 @@ def breakup(dataset_loc,pars_loc,destination):
         locations.append(loc)
     
     locations = np.asarray(locations)
-    np.savetxt("data/spectra_loc.txt",locations)
+    np.savetxt("data/spectra_loc.txt",locations, fmt='%s')
     
 
 def main():
