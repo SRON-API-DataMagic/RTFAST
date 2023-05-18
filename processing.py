3b25d476eb524f1e81741bc6a9d469313c98df8d
benr <b.ricketts@sron.nl>
2023-05-18 13:50:02 +0200

    Created way to break up spectra into individual files so can be later loaded directly from disk in training

--- processing.py
+++ processing.py
@@ -1,3 +1,32 @@
 """
-This module generally deals with data processing for plotting
+This module generally deals with data processing for plotting and retrieval
 """
+
+import numpy as np
+
+def breakup(dataset_loc,pars_loc,destination):
+    dataset = np.loadtxt(dataset_loc)
+    pars = np.loadtxt(pars_loc)
+    
+    locations = np.zeros(shape = (pars.shape[0],pars.shape[1]+1))
+    for i,(spectra,pars) in enumerate(zip(dataset,pars)):
+        loc = f"data/spectra/spectra_{i}.txt"
+        print(loc)
+        print(spectra)
+        print(np.concatenate((pars,np.array([loc]))))
+        break
+        np.savetxt(loc,spectra)
+        locations[i] = np.concatenate((pars,np.array([loc])))
+    
+    locations = np.asarray(locations)
+    np.savetxt("data/locations/spectra_loc_30.txt",locations)
+    
+
+def main():
+    dataset_loc = "data/loop_30_data.txt"
+    pars_loc = "data/loop_30_pars.txt"
+    destination = "data/spectra"
+    breakup(dataset_loc, pars_loc, destination)
+
+if __name__ == "__main__":
+    main()
