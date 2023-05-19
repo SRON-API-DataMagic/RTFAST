398840b4ef9500b8c929ee91531b1bac34f0b452
benr <b.ricketts@sron.nl>
2023-05-19 12:06:03 +0200

    Reprocessing so locations and parameters are connected together in one csv file per save

--- processing.py
+++ processing.py
@@ -7,7 +7,7 @@ import tqdm
 import glob
 import pandas as pd
 
-def breakup(dataset_loc,pars_loc,destination):
+def breakup(dataset_loc, pars_loc, destination):
     dataset = np.loadtxt(dataset_loc)
     
     files = glob.glob("./data/spectra/*.txt")
@@ -28,23 +28,21 @@ def breakup(dataset_loc,pars_loc,destination):
     np.savetxt("data/locations/test_loc.txt",locations, fmt='%s')
     return
 
-def merging_locations_pars(pars_loc,locations_loc,destination):
+def merging_locations_pars(pars_loc, locations_loc, destination,index):
     pars = np.loadtxt(pars_loc)
     locations = np.loadtxt(locations_loc,dtype=str)
     column_names = ["h","a","inc","rin","rout","z","Gamma","Dkpc","Afe","logNe",
                     "kTe","nH","boost","Mass","honr","b1","b2","fmin","fmax",
                     "ReIm","phiA","phiAB","g","Anorm","RESP","Xnorm"]
     pars_df = pd.DataFrame(pars,columns = column_names)
-    print(pars_df)
     locations_df = pd.DataFrame(locations,columns=["Location"])
-    print(locations_df)
     final_df = pd.concat([pars_df,locations_df],axis = 1, join = "inner")
-    print(final_df)
+    final_df.to_csv(destination+"loc_"+index+".csv")
     return
     
 
 def main():
-    destination = "data/"
+    destination = "data/locations/"
     locations_loc ="data/locations/loc_"
     pars_loc = "data/pars/"
     
@@ -52,9 +50,10 @@ def main():
                "grid_7","grid_8","grid_9","grid_10"]
     
     for index in indexes:
+        print(index)
         loc_index = locations_loc+index+".txt"
         pars_index = pars_loc+index+"_pars.txt"
-        merging_locations_pars(pars_index, loc_index, destination)
+        merging_locations_pars(pars_index, loc_index, destination, index)
     
 if __name__ == "__main__":
     main()
