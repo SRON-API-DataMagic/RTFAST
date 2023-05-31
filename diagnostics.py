6a698250957adfd0c5bc14c395bcc17b1ccd34e2
benr <b.ricketts@sron.nl>
2023-05-31 15:42:28 +0200

    Fix to diagnostics

--- diagnostics.py
+++ diagnostics.py
@@ -11,7 +11,12 @@ import torch.autograd.profiler as profiler
 from network import NeuralNetwork
 from main import maskedMSELoss
 
+print(torch.cuda.is_available())
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
+
 model = NeuralNetwork(5, 4096)
+model.to(device)
 
 inp = torch.rand(128, 5).cuda()
 mask = torch.randint(0,2,(128, 4096)).cuda()
@@ -20,7 +25,7 @@ data = torch.rand(128,4096).cuda()
 model(inp)
 
 with profiler.profile(with_stack=True, profile_memory=True) as prof:
-    pred = model(inp)
-    loss = maskedMSELoss(pred, data, mask)
+    pred = model(inp.to(device))
+    loss = maskedMSELoss(pred, data.to(device), mask.to(device))
 
 print(prof.key_averages(group_by_stack_n=5).table(sort_by='self_cpu_time_total', row_limit=5))
