a0e33be30ddf953de82a3c5ee9f17ff4c602e89b
benr <b.ricketts@sron.nl>
2023-06-01 13:52:25 +0200

    Batch size reduction to see CPU speed improvemnt

--- diagnostics.py
+++ diagnostics.py
@@ -29,7 +29,7 @@ fname = "grid_7"
 locations = "data/locations/"
 scaler = MinMaxScaler()
 num_workers = 4
-batch_size = 8192
+batch_size = 512
 
 optimizer = Adam(model.parameters(),lr = 0.001)
 loss_fn = maskedMSELoss
