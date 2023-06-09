6b6454bdae02929db30a5e7ae3a3758bf7d0b71e
benr <b.ricketts@sron.nl>
2023-06-09 09:13:42 +0200

    Saved device as attribute

--- main.py
+++ main.py
@@ -230,6 +230,7 @@ class barredMSELoss(nn.Module):
     def __init__(self,scaler,device):
         super(barredMSELoss, self).__init__()
         self.scaler = load(f'scalers/{scaler}')
+        self.device = device
         self.set_scale()
         
     def set_scale(self):
