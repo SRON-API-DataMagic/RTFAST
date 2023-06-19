96914b72d25cc6322c1f5c9abedd35c818074185
benr <b.ricketts@sron.nl>
2023-06-19 13:09:27 +0200

    Fix

--- plotting.py
+++ plotting.py
@@ -484,7 +484,8 @@ def energy_plots(dataset,scaler,model,egrid):
     rins = []
     routs = []
     index_start = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
-    indexes = (int(i*len(egrid)) for i in index_start)
+    indexes = [int(i*len(egrid)) for i in index_start]
+    print(indexes)
     values = egrid[indexes]
     for batch, (D,P,M) in enumerate(dataset):
         M = np.squeeze(M)
