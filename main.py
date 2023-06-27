6ec56910d96e815ba588124f5993be9f7ac9080e
benr <b.ricketts@sron.nl>
2023-06-27 12:33:15 +0200

    Train grids on new network structure

--- main.py
+++ main.py
@@ -754,8 +754,8 @@ def main():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     print(device)
     
-    queryByDropout(wrk_dir,device)
-    #grid(wrk_dir,device)
+    #queryByDropout(wrk_dir,device)
+    grid(wrk_dir,device)
 
 if __name__ == "__main__":
     main()
