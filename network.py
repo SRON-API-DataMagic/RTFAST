511dd367e8de9e8aa466ae9e181a85a38d448c99
benr <b.ricketts@sron.nl>
2023-07-10 10:08:35 +0200

    Added another layer to Sharp network

--- network.py
+++ network.py
@@ -98,10 +98,24 @@ class SharpNetwork(NeuralNetwork):
             nn.Linear(256,512),
             SharpActivation(512))
         self.LinearStack3 = nn.Sequential(
-            nn.Linear(512,data_len))
+            nn.Linear(512,1024),
+            SharpActivation(1024))
+        self.LinearStack4 = nn.Sequential(
+            nn.Linear(1024,data_len))
         self.dropout1 = nn.Dropout(p=self.p)
         self.dropout2 = nn.Dropout(p=self.p)
+        self.dropout3 = nn.Dropout(p=self.p)
         self.double()
+    
+    def forward(self,pars):
+        stack1 = self.LinearStack1(pars)
+        stack1 = self.dropout1(stack1)
+        stack2 = self.LinearStack2(stack1)
+        stack2 = self.dropout2(stack2)
+        stack3 = self.LinearStack3(stack2)
+        stack3 = self.dropout3(stack3)
+        result = self.LinearStack4(stack3)
+        return result
 
 class Committee(NeuralNetwork):
     """
