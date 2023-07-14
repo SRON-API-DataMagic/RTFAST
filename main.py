d1e1dd4cee2172dd5083ff07c2f04f67f5b47f1b
benr <b.ricketts@sron.nl>
2023-07-14 17:39:31 +0200

    print largest time lag

--- main.py
+++ main.py
@@ -328,6 +328,7 @@ def queryByDropout(wrk_dir, device = None):
         lags_data_init = np.array(flux_data_init)
         
         print(np.count_nonzero(np.isinf(lags_data_init)))
+        print(np.amax(lags_data_init))
         
         #check for and delete parameter sets producing NaN results for flux
         index = nanChecker(flux_data_init, theta_init)
