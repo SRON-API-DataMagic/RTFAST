1e24eaa54902abe6c53f61b3d74458b1b6477a74
benr <b.ricketts@sron.nl>
2023-07-17 15:40:55 +0200

    Fixed inheritance in LagsData

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ class FluxData(Dataset):
 class LagsData(FluxData):
     
     def __init__(self,labels, scaler, scaler_name, scaling=False):
-        super().__init__()
+        super(LagsData, self).__init__(labels, scaler, scaler_name, scaling)
         self.labels = pd.read_csv(labels)
         self.pars_list = [1,13,2,3,4]
         self.scaling = scaling
