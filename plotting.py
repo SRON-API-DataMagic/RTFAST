e38cc35beec4f4da381d7db5f8e03bce39988d4b
benr <b.ricketts@sron.nl>
2023-08-14 14:18:36 +0200

    Minor bug fix

--- plotting.py
+++ plotting.py
@@ -823,9 +823,9 @@ def main():
                                     num_workers=1)
     
     mname = "compare/Comparing_val"
-    model_samples(testing_dataloader, scaler, model, lags_egrid, mname, mode, no_brk=True)
+    model_samples(testing_dataloader, scaler, model, lags_egrid[:-1], mname, mode, no_brk=True)
     mname = "compare/Comparing_tra"
-    model_samples(training_dataloader, scaler, model, lags_egrid, mname, mode, no_brk=True)
+    model_samples(training_dataloader, scaler, model, lags_egrid[:-1], mname, mode, no_brk=True)
     
     
     set_envir_vars(wrk_dir)
