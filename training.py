ede65d0fa0c7bcec919f089a763fba30940a6cd9
benr <b.ricketts@sron.nl>
2023-09-06 16:44:07 +0200

    Added test to see if indices output from network is incorrect

--- training.py
+++ training.py
@@ -156,6 +156,8 @@ class lagLoss(nn.Module):
         data = torch.mul(target,mask)
         #calculate loss
         loss = self.criterion(pred,data)
+        print(torch.any((index < 0)|(index > 1)))
+        print(torch.any((index_target < 0)|(index_target > 1)))
         signed_loss = self.binary(index,index_target)
         loss += signed_loss
         return loss 
