4edafa38360b8242afe9c1ec7f9350f735ae67f0
benr <b.ricketts@sron.nl>
2023-09-18 14:29:35 +0200

    file fix

--- plotting.py
+++ plotting.py
@@ -885,7 +885,7 @@ def main():
     wrk_dir = os.getcwd()
     
     nums = [0,10,20,30,40]
-    files = [f"loop_{loop}_variances.txt" for loop in nums]
+    files = [f"dists/loop_{loop}_variances.txt" for loop in nums]
     aggregate_dists(files)
     
     egrid = retrieve_egrid(wrk_dir)
