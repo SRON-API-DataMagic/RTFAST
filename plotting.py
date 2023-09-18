0a515a9fc176c5ef45817546dd997b7e2dea32a7
benr <b.ricketts@sron.nl>
2023-09-18 14:33:52 +0200

    file fix

--- plotting.py
+++ plotting.py
@@ -885,7 +885,7 @@ def main():
     wrk_dir = os.getcwd()
     
     nums = [0,10,20,30,40]
-    files = [f"data/locations/loc_flux_{loop}.txt" for loop in nums]
+    files = [f"data/locations/loc_flux_{loop}.csv" for loop in nums]
     aggregate_dists(files)
     
     egrid = retrieve_egrid(wrk_dir)
