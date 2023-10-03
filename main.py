25c90041e2fcda0d60bdb1bd38a44cc81aeabeaf
benr <b.ricketts@sron.nl>
2023-10-03 10:52:28 +0200

    Train 10 size grid model onwards

--- main.py
+++ main.py
@@ -304,6 +304,7 @@ def grid_learning(wrk_dir,device):
     for size in grid_sizes:
         grid_names.append(f"grid_{size}")
     print(grid_names)
+    grid_sizes = [10]
         
     batch_size = 1024
     num_workers = 4
@@ -398,17 +399,6 @@ def main():
     
     print(torch.cuda.is_available())
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    
-    sizes = [5,6,7,8,9,10]
-    fnames = [f"grid_{i}" for i in sizes]
-    
-    rmf_name = wrk_dir+"/ResponseFiles/PN.rmf"
-    rmf = unpack_rmf(rmf_name)
-    egrid = rmf.e_min #energy grid used to evaluate the xspec model
-    lags_egrid = np.logspace(np.log10(0.5),np.log10(11),num=26)
-    
-    for size, fname in zip(sizes,fnames):
-        grid_data_gen(size, fname, egrid, lags_egrid)
     
     grid_learning(wrk_dir,device)
     active_learning(wrk_dir,device)
