6fbb1ceb8692ed011843c3d6760ba38d0575141f
benr <b.ricketts@sron.nl>
2023-10-09 13:19:38 +0200

    Run on cpu for better debugging

--- main.py
+++ main.py
@@ -426,6 +426,7 @@ def main():
     
     print(torch.cuda.is_available())
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    device = 'cpu'
     
     #grid_learning(wrk_dir,device)
     active_learning(wrk_dir,device)
