3a67ad64fb92f350ad0b748e0f4493d3d5b63abc
benr <b.ricketts@sron.nl>
2023-10-10 15:53:06 +0200

    Actually generate new test set and save itg

--- plotting.py
+++ plotting.py
@@ -861,6 +861,11 @@ def main():
     
     egrid = retrieve_egrid(wrk_dir)
     lags_egrid = np.logspace(np.log10(0.5),np.log10(11),num=26)
+    flux, lags, theta_flux, theta_lags = generate_test_set(500, egrid, lags_egrid)
+    
+    saveData(flux, theta_flux, "data/locations", "loc_flux_test.csv")
+    saveData(lags, theta_lags, "data/locations", "loc_lags_test.csv")
+    
     
     set_envir_vars(wrk_dir)
     active_v_grid(wrk_dir, egrid, lags_egrid)
