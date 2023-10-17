b9210b0854c01e93ad1d908a6eb0878c988194e5
benr <b.ricketts@sron.nl>
2023-10-17 12:23:34 +0200

    Active learning first

--- main.py
+++ main.py
@@ -430,8 +430,9 @@ def main():
     
     print("Cuda is available:",torch.cuda.is_available())
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    grid_learning(wrk_dir,device)
+    
     active_learning(wrk_dir,device)
+    grid_learning(wrk_dir,device)
     
 
 if __name__ == "__main__":
