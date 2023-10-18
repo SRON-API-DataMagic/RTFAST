24e5e1b48e8fcc91178c6f359b58a9ab19711af6
benr <b.ricketts@sron.nl>
2023-10-18 09:30:43 +0200

    Egrid bug fix

--- generator.py
+++ generator.py
@@ -30,7 +30,7 @@ def rtdist_erg_flux(pars, egrid):
     model = _models.tdrtdist(pars, egrid)
     #take middle energy and multiply all photon counts by said energy
     gmid = 1.60217653e-09  * (egrid[:-1] + egrid[1:]) / 2
-    model = model*gmid
+    model = model[:-1]*gmid
     interp = scipy.interpolate.InterpolatedUnivariateSpline(egrid, model, k=1)
     flux = interp.integral(interp, 2, 10, args=(model,egrid))
     return flux
