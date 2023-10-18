36ee7028612641eacb18d842cb1fba35258dcb76
benr <b.ricketts@sron.nl>
2023-10-18 14:00:02 +0200

    Debugging

--- main.py
+++ main.py
@@ -72,7 +72,8 @@ def active_learning(wrk_dir, device = "cpu"):
     flux_bh = flux_bh[np.isnan(flux_bh)==False]
     flux_agn = flux_agn[np.isnan(flux_agn)==False]
     
-    print(flux_bh)
+    print(flux_bh.max())
+    print(flux_agn.max())
     
     bh_bins = np.logspace(np.min(flux_bh),np.max(flux_bh),100)
     agn_bins = np.logspace(np.min(flux_agn),np.max(flux_agn),100)
