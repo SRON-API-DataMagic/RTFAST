1ffe525e5a03e0a263b4a0c22d01841bfa2a0285
benr <b.ricketts@sron.nl>
2023-10-19 13:41:57 +0200

    Convert parameters from random sample to input

--- plotting.py
+++ plotting.py
@@ -207,6 +207,11 @@ def plot_flux_dists():
     
     theta_bh = generator.lhs_generation(5000, range_BH)
     theta_agn = generator.lhs_generation(5000, range_AGN)
+    
+    
+    theta_bh = generator.pars_conversion(theta_bh,0)
+    theta_agn = generator.pars_conversion(theta_agn,0)
+    
     with Parallel(n_jobs=10,verbose=5) as parallel:
         #generate rtdist models for the correlated grid
         flux_BH = parallel(delayed(generator.rtdist_erg_flux)(pars, egrid)
