1c6cd792045f685b73949b60812f2b522dcc04f1
benr <b.ricketts@sron.nl>
2023-10-20 11:48:44 +0200

    Actually generate new values

--- plotting.py
+++ plotting.py
@@ -204,7 +204,7 @@ def plot_flux_dists():
     
     BH_name = "5h_14G_BH"
     AGN_name = "5h_14G_AGN"
-    #generator.generate_flux_dists(BH_name, AGN_name)
+    generator.generate_flux_dists(BH_name, AGN_name)
     
     BHs = pd.read_csv(f"data/flux/{BH_name}.csv")
     AGN = pd.read_csv(f"data/flux/{AGN_name}.csv")
