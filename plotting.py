ef2b5e5c9f96b493e7eb76908693ae16f46ec89f
benr <b.ricketts@sron.nl>
2023-10-25 09:39:24 +0200

    Set z-axis to log

--- plotting.py
+++ plotting.py
@@ -231,6 +231,7 @@ def plot_flux_dists():
     ax.plot(x_0,y_2,zs=1e-11,c="orange",ls="--")
     ax.plot(x_1,y_0,zs=1e-11,c="orange",ls="--")
     ax.plot(x_2,y_0,zs=1e-11,c="orange",ls="--")
+    ax.zaxis.set_scale("log")
     fig.colorbar(p, label="Photon index")
     fig.tight_layout()
     plt.savefig("data/flux/rin_h_gamma_AGN.png")
