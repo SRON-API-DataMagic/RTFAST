15879d5cac4b66fc7e14e44e7924bf4d37251693
benr <b.ricketts@sron.nl>
2023-10-25 10:18:38 +0200

    Add transparency to surface

--- plotting.py
+++ plotting.py
@@ -226,8 +226,10 @@ def plot_flux_dists():
         y_1 = np.ones(50)*AGN["rin"].min()
         y_2 = np.ones(50)*AGN["rin"].max()
         X, Y = np.meshgrid(x_0,y_0)
-        ax.plot_surface(X, Y, -6*np.ones((len(X),len(Y))), linewidth=0.5)
-        ax.plot_surface(X, Y, -11*np.ones((len(X),len(Y))), linewidth=0.5)
+        ax.plot_surface(X, Y, -6*np.ones((len(X),len(Y))), linewidth=0.5,
+                        alpha = 0.3)
+        ax.plot_surface(X, Y, -11*np.ones((len(X),len(Y))), linewidth=0.5,
+                        alpha = 0.3)
         #plot flux upper threshold
         ax.plot(x_0,y_1,zs=-6,c="orange",ls="--")
         ax.plot(x_0,y_2,zs=-6,c="orange",ls="--")
