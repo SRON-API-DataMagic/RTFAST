cec3a785f946d323f6baef6fc6f7c7661ee92980
benr <b.ricketts@sron.nl>
2023-10-30 13:44:15 +0100

    Adjust ticks

--- plotting.py
+++ plotting.py
@@ -388,15 +388,20 @@ def plot_flux_ranges_corner():
     for i in range(len(labels)):
         plt.setp(axs[-1, i], xlabel=labels[i])
         plt.setp(axs[i, 0], ylabel=labels[i])
-        if i != len(labels)-1:
-            #hide tick labels
-            axs[i,0].xaxis.set_tick_params(labelbottom=False)
-            axs[-1,i].yaxis.set_tick_params(labelleft=False)
-            
-            # Hide X and Y axes tick marks
-            axs[i,0].set_xticks([])
-            axs[-1,i].set_yticks([])
-            
+    
+    for i in range(len(labels)):
+        for j in range(len(labels)):
+            if i != 0 and j !=len(labels)-1:
+                axs[i,j].xaxis.set_tick_params(labelbottom=False)
+                axs[i,j].yaxis.set_tick_params(labelleft=False)
+                axs[i,j].set_xticks([])
+                axs[i,j].set_yticks([])
+            elif i == 0 and j !=len(labels)-1:
+                axs[i,j].xaxis.set_tick_params(labelbottom=False)
+                axs[i,j].set_xticks([])
+            elif i != 0 and j ==len(labels)-1:
+                axs[i,j].yaxis.set_tick_params(labelleft=False)
+                axs[i,j].set_yticks([])
     for i in range(len(labels)):
         for j in range(len(labels)):
             if j>=i:
