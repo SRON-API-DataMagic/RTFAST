52a6734ed6c900aebeeb3215351f8e78a0394625
benr <b.ricketts@sron.nl>
2023-10-30 13:46:17 +0100

    Adjust ticks

--- plotting.py
+++ plotting.py
@@ -391,15 +391,15 @@ def plot_flux_ranges_corner():
     
     for i in range(len(labels)):
         for j in range(len(labels)):
-            if i != 0 and j !=len(labels)-1:
+            if i != 0 and j !=len(labels):
                 axs[i,j].xaxis.set_tick_params(labelbottom=False)
                 axs[i,j].yaxis.set_tick_params(labelleft=False)
                 axs[i,j].set_xticks([])
                 axs[i,j].set_yticks([])
-            elif i == 0 and j !=len(labels)-1:
+            elif i == 0 and j !=len(labels):
                 axs[i,j].xaxis.set_tick_params(labelbottom=False)
                 axs[i,j].set_xticks([])
-            elif i != 0 and j ==len(labels)-1:
+            elif i != 0 and j ==len(labels):
                 axs[i,j].yaxis.set_tick_params(labelleft=False)
                 axs[i,j].set_yticks([])
     for i in range(len(labels)):
