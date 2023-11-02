921aba4b463579ca389b89ab1f1b74e7705c699d
benr <b.ricketts@sron.nl>
2023-11-02 11:48:29 +0100

    Debugging

--- plotting.py
+++ plotting.py
@@ -940,8 +940,8 @@ def aggregate_dists(files):
     for i, item in enumerate(pars_list):
         x = np.floor(i/5)
         y = i%5
-        axs[x,y].hist(full_pars[i], pars_bins[i], histtype="bar", stacked=True, 
-                                 color = cmap, label=labels)
+        axs[x,y].hist(np.squeeze(full_pars[i]), np.squeeze(pars_bins[i]), 
+                      histtype="bar", stacked=True, color = cmap, label=labels)
         if i in logged:
             axs[x,y].set_xscale("log")
         axs[x,y].set_xlabel(pars_list[i])
