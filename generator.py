Change to correct units?

generator.py
@@ -118,11 +118,11 @@ def Anorm_wrapper(pars):
     print("Calculating normalisations")
     #energies from 0.1keV to 1MeV
     egrid = np.linspace(1e-1,1e3,num = 10000)
-    E_cut = 1.60217653e-09 * 300   #cutoff in ergs
+    E_cut = 300                     #cutoff in keV
     fluxs = np.zeros((pars.shape[0],egrid.shape[0]-1))
     
     for i in range(fluxs.shape[1]):
-        E_mid = 1.60217653e-09 * (egrid[i]+egrid[i+1])/2
+        E_mid = (egrid[i]+egrid[i+1])/2
         fluxs[:,i] = np.exp((-0.5*E_mid)/E_cut)*E_mid**(1-gamma)
     
     normal = 10**20 * 10**15 /(4*np.pi)
@@ -135,13 +135,13 @@ def Anorm_wrapper(pars):
     print("Integrating fluxes")
     integrals = np.zeros(normalisation.shape)
     E_range = np.linspace(1e-3,1e4,num = 1000)
-    gmid = 1.60217653e-09  * (E_range[:-1] + E_range[1:]) / 2
+    gmid = (E_range[:-1] + E_range[1:]) / 2
     
     for i in range(len(normalisation)):
         fluxs = normalisation[i]*flux(gmid,gamma[i])
         fluxs = fluxs.sum()
         integrals[i] = fluxs
-    Anorm = (3.086e21**2)*(L/(8*np.pi*D**2*g_so**(gamma-2)*integrals))
+    Anorm = L/(8*np.pi*D**2*g_so**(gamma-2)*integrals)
     pars[:,19] = Anorm   #Replace flux generated with Anorm parameters
     print("Calculated Anorms")
     print("See L/Ledds for comparison:")