8a931a6f51b4e861ee25312ca3b6c5282cf91c30
benr <b.ricketts@sron.nl>
2023-11-16 14:20:38 +0100

    Directly compare bins

--- generator.py
+++ generator.py
@@ -139,17 +139,17 @@ def Anorm_wrapper(pars):
         print(xill_pars)
         print(xill_pars.shape)
         
-        for i in range(xill_pars.shape[1]):
-            continuum = _models.lmodxillver(xill_pars[:,i], egrid)[:-1]
-            plt.plot(egrid[:-1],continuum)
-            plt.xscale("log")
-            plt.yscale("log")
-            plt.ylabel("Continuum (Photons cm^-2 s^-1 KeV^-1)")
-            plt.xlabel("Energy (keV)")
-            plt.xlim(0.1,40)
-            plt.ylim(6,6200)
-            plt.savefig(f"verify/{i}_{binz}.pdf")
-            integrals[i] = (continuum*e_mid).sum()
+        continuum = _models.lmodxillver(xill_pars[:,0], egrid)[:-1]
+        plt.plot(egrid[:-1],continuum)
+        plt.xscale("log")
+        plt.yscale("log")
+        plt.ylabel("Continuum (Photons cm^-2 s^-1 KeV^-1)")
+        plt.xlabel("Energy (keV)")
+        plt.xlim(0.1,40)
+        plt.ylim(6,6200)
+        plt.savefig(f"verify/{binz}.pdf")
+        plt.close()
+        integrals[0] = (continuum*e_mid).sum()
     
     Anorm = F/(g_so**(gamma-2)*integrals)
     pars[:,19] = Anorm   #Replace flux generated with Anorm parameters
