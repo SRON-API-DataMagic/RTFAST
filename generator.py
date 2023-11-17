1ad3c52c87a8e5e9d8e6252f6368d7dbc6a9cbf9
benr <b.ricketts@sron.nl>
2023-11-17 16:25:13 +0100

    Fix concatenation of lhc sets

--- generator.py
+++ generator.py
@@ -767,7 +767,7 @@ def lhc_generation(size,range_all):
         print(f"Currently {lhc.shape[0]}/{size}.")
         print("Generating more parmaters...")
         lhc_temp = lhc_cycle(size, range_all)
-        lhc = np.concatenate((lhc,lhc_temp),axis=1)
+        lhc = np.concatenate((lhc,lhc_temp),axis=0)
     np.random.shuffle(lhc)
     lhc = lhc[:size]
     return lhc
