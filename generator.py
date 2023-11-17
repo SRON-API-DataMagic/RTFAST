0d6b256b80c4428fdd227f79c7347a0c8379ce37
benr <b.ricketts@sron.nl>
2023-11-17 16:21:53 +0100

    Debug

--- generator.py
+++ generator.py
@@ -767,7 +767,10 @@ def lhc_generation(size,range_all):
         print(f"Currently {lhc.shape[0]}/{size}.")
         print("Generating more parmaters...")
         lhc_temp = lhc_generation(size, range_all)
+        print(lhc_temp.shape)
+        print(lhc.shape)
         lhc = np.concatenate((lhc,lhc_temp),axis=1)
+        print(lhc.shape)
     np.random.shuffle(lhc)
     lhc = lhc[:size]
     return lhc
