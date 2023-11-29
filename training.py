a5a3ada546f1ac4ccd404bf5042f15d16c1e283b
benr <b.ricketts@sron.nl>
2023-11-29 09:15:11 +0100

    Place tensors correctly for lags loss

--- training.py
+++ training.py
@@ -124,7 +124,7 @@ class FluxLoss(nn.Module):
             result = 10**result
             return result
         elif self.scale_type == "Standard":
-            result = (a - self.mean)/self.scale
+            result = (a - self.mean.to(self.device))/self.scale.to(self.device)
             return 10**result
         
     def forward(self, output, target):
