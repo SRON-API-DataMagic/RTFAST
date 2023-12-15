88acdde175674ce6924171e68832d429a6dd04d5
benr <b.ricketts@sron.nl>
2023-12-15 09:56:59 +0100

    Plot to the 40th loop

--- plotting.py
+++ plotting.py
@@ -1090,7 +1090,7 @@ def active_v_grid(wrk_dir, egrid, lags_egrid):
     model_base_loc = wrk_dir+"/models/"
     loss_base_loc = wrk_dir+"/loss/"
     
-    active_name = [0,10,20,30,36]
+    active_name = [0,10,20,30,40]
     active_name = np.array(active_name)
     active_sample_flux_nums = []
     active_sample_lags_nums = []
