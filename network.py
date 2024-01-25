0278e8b3ac55d256b955748c2b9ad218c79f9b96
benr <b.ricketts@sron.nl>
2024-01-25 09:05:21 +0100

    Made network larger by one layer

--- network.py
+++ network.py
@@ -265,7 +265,9 @@ class PCAFluxNetwork(nn.Module):
     
     def __init__(self,num_pars,output_len):
         super().__init__()
-        self.LinearStack = nn.Linear(num_pars, output_len)
+        self.LinearStack = nn.Sequential(nn.Linear(num_pars, 20),
+                                         nn.ReLU(),
+                                         nn.Linear(20, output_len))
     
     def forward(self,pars):
         return self.LinearStack(pars)
