7429af1b258d5d99b0fed4451a089986871f6abd
benr <b.ricketts@sron.nl>
2024-01-25 13:09:00 +0100

    Dynamic PCA parameter axis assignment

--- plotting.py
+++ plotting.py
@@ -1224,7 +1224,7 @@ def PCA_plotting(wrk_dir):
         plt.close()
         
         pca_da = pca.transform(scaler.transform(np.log10(da.reshape(1, -1))))
-        x = np.arange(0,len(pars_list))
+        x = np.arange(0,len(pars_list)-1)
         plt.scatter(x,pca_da,label="True PCA components")
         plt.scatter(x,pred,label="Emulator PCA components")
         plt.xlabel("PCA component")
