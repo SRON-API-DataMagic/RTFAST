58ca32ca8854994c1af81ab56b0d93de44c3329e
benr <b.ricketts@sron.nl>
2024-01-26 11:49:48 +0100

    removed scaling of log spectra

--- dataStructures.py
+++ dataStructures.py
@@ -532,19 +532,8 @@ class PCADataset(Dataset):
         self.scale()
     
     def scale(self):
-        #self.flux_scaler()
         self.PCA()
         self.component_scaler()
-        return
-        
-    def flux_scaler(self):
-        if self.scale_bool == True:
-            self.flux_scaler = MinMaxScaler()
-            self.data = self.flux_scaler.fit_transform(self.data)
-            dump(self.flux_scaler,"scalers/flux_scaler.bin")
-        else:
-            self.flux_scaler = load("scalers/flux_scaler.bin")
-            self.data = self.flux_scaler.transform(self.data)
         return
     
     def PCA(self,comp = 1):
