3c8363d537ed352c9c85bd6ef8316666951d2955
benr <b.ricketts@sron.nl>
2024-01-29 11:27:44 +0100

    Removed reshuffle

--- test.py
+++ test.py
@@ -46,23 +46,6 @@ logged = [2]
 range_AGN = np.asarray(generator.lhc_2())
 num_pars = range_AGN.shape[0]
 
-train = pd.read_csv("PCA_locs_flux.csv")
-val = pd.read_csv("PCA_locs_flux_test.csv")
-
-flux = pd.concat([train,val],ignore_index=True)
-
-idxs = np.arange(0,flux.shape[0])
-np.random.shuffle(idxs)
-tra_idx = idxs[:int(0.9*len(idxs))]
-tes_idx = idxs[int(0.9*len(idxs)):]
-
-#Splitting data and parameters into training and testing datasets
-train_flux_data = flux[tra_idx]
-train_flux_data.to_csv("data/locations/PCA_locs_flux.csv",index=False)
-
-test_flux_data = flux[tes_idx]
-test_flux_data.to_csv("data/locations/PCA_locs_flux_test.csv",index=False)
-
 train_dataset = PCADataset("data/locations/PCA_locs_flux.csv",
                            pars_list,negatives,logged,scale_bool = False)
 train_dataloader = DataLoader(train_dataset, batch_size=1024, num_workers = 4, 
