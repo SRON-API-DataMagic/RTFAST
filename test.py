31a4f3ff68cbea259f25245749eac537fa1d1d73
benr <b.ricketts@sron.nl>
2024-01-30 10:39:22 +0100

    Rescale to reduce number of PCA components

--- test.py
+++ test.py
@@ -80,7 +80,7 @@ saveData(test_flux_data, test_flux_pars,
          "data/locations/","PCA_locs_flux_test.csv")
 """
 train_dataset = PCADataset("data/locations/PCA_locs_flux.csv",
-                           pars_list,negatives,logged,scale_bool = False)
+                           pars_list,negatives,logged)
 train_dataloader = DataLoader(train_dataset, batch_size=1024, num_workers = 4, 
                               shuffle=True)
 
