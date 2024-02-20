34498fa6af3772d5da01ce310004c387ec7985b3
benr <b.ricketts@sron.nl>
2024-02-20 12:10:00 +0100

    Plot 5 parameter case

--- plotting.py
+++ plotting.py
@@ -1172,6 +1172,7 @@ def PCA_plotting(wrk_dir,name):
     plt.close()
     
     #plotting of emulator vs test data performance
+    """
     pars_list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,21,22,23]
     negatives = [3]
     logged = [0,2,3,4,7,8,10,11,12,13,23]
@@ -1180,7 +1181,6 @@ def PCA_plotting(wrk_dir,name):
     negatives = [3]
     logged = [2,3,4,13]
     """
-    """
     pars_list = [3]
     negatives = [3]
     logged = [3]
@@ -1188,8 +1188,10 @@ def PCA_plotting(wrk_dir,name):
     print(len(pars_list))
     labels = ["a","inc","rin","rout","mass"]
     #labels = ["rin"]
+    """
     labels = ["height","a","inc","rin","rout","z","Gamma","distance","Afe","logNe","kte",
               "nH","boost","mass","honr","b1","b2","phiAB","g","Anorm"]
+    """
     test_data = PCADataset("data/locations/loc_flux_test.csv",
                                pars_list,negatives,logged,scale_bool = False,
                                PCA_loc=f"scalers/PCA_flux.bin",
@@ -1377,13 +1379,13 @@ def main():
     
     egrid = retrieve_egrid(wrk_dir)
     lags_egrid = np.logspace(np.log10(0.5),np.log10(11),num=26)
-    range_AGN = np.asarray(generator.lhc_AGN())
+    range_AGN = np.asarray(generator.lhc_trimmed_gen())
     num_pars = range_AGN.shape[0]
     
-    theta_lhc = generator.lhc_generation(int(1e3), range_AGN, limited=False)
-    pars_list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,21,22,23]
+    theta_lhc = generator.lhc_generation(int(1e3), range_AGN, limited=True)
+    pars_list = [1,2,3,4,13]
     negatives = [3]
-    logged = [0,2,3,4,7,8,10,11,12,13,23]
+    logged = [2,3,4,13]
     theta_flux = generator.nn_pars_to_rtdist(theta_lhc, 0, pars_list, negatives, logged)
     theta_lags = generator.nn_pars_to_rtdist(theta_lhc, 0, pars_list, negatives, logged)
     print("Parallelized model generation")
