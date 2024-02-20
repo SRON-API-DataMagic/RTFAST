474d029a487f4f1ae2004274038539e61022d86b
benr <b.ricketts@sron.nl>
2024-02-20 13:15:50 +0100

    Double depth of network

--- network.py
+++ network.py
@@ -271,6 +271,10 @@ class PCAFluxNetwork(nn.Module):
                                          nn.ReLU(),
                                          nn.Linear(20, 20),
                                          nn.ReLU(),
+                                         nn.Linear(20, 20),
+                                         nn.ReLU(),
+                                         nn.Linear(20, 20),
+                                         nn.ReLU(),
                                          nn.Linear(20, output_len))
     
     def forward(self,pars):
