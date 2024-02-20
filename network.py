b052f5d63f0d5c26458fd849cbbddc897bdde10d
benr <b.ricketts@sron.nl>
2024-02-20 16:03:18 +0100

    Compression to wider network

--- network.py
+++ network.py
@@ -265,17 +265,13 @@ class PCAFluxNetwork(nn.Module):
     
     def __init__(self,num_pars,output_len):
         super().__init__()
-        self.LinearStack = nn.Sequential(nn.Linear(num_pars, 40),
+        self.LinearStack = nn.Sequential(nn.Linear(num_pars, 60),
                                          nn.ReLU(),
-                                         nn.Linear(40, 40),
+                                         nn.Linear(60, 60),
                                          nn.ReLU(),
-                                         nn.Linear(40, 40),
+                                         nn.Linear(60, 60),
                                          nn.ReLU(),
-                                         nn.Linear(40, 40),
-                                         nn.ReLU(),
-                                         nn.Linear(40, 40),
-                                         nn.ReLU(),
-                                         nn.Linear(40, output_len))
+                                         nn.Linear(60, output_len))
     
     def forward(self,pars):
         return self.LinearStack(pars)
