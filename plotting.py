7ea16c2f13df9038b4341e4ccc8bac83adbb07a1
benr <b.ricketts@sron.nl>
2024-02-20 12:13:23 +0100

    Reconstruct emulator fix

--- plotting.py
+++ plotting.py
@@ -1362,7 +1362,7 @@ def PCA_plotting(wrk_dir,name):
             plt.close()
 
 def reconstruct_emulator(dataset,model):
-    pred = model(dataset.pars).detach().numpy().reshape(-1, 1)
+    pred = model(dataset.pars).detach().numpy().reshape(1, -1)
     pred = dataset.PCA_scaler.inverse_transform(pred)
     pred = dataset.pca.inverse_transform(pred)
     pred = 10**pred
