0656a563592836bf0a54e6e8a6b9e73c4e32e166
benr <b.ricketts@sron.nl>
2024-02-20 15:30:53 +0100

    Slightly wider network

--- network.py
+++ network.py
@@ -271,6 +271,8 @@ class PCAFluxNetwork(nn.Module):
                                          nn.ReLU(),
                                          nn.Linear(40, 40),
                                          nn.ReLU(),
+                                         nn.Linear(40, 40),
+                                         nn.ReLU(),
                                          nn.Linear(40, output_len))
     
     def forward(self,pars):
