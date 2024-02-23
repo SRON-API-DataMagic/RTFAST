8da768f499cbf5159ea6df08d6b0315d3f60997a
benr <b.ricketts@sron.nl>
2024-02-23 13:11:15 +0100

    Tdqm data loading

--- plotting.py
+++ plotting.py
@@ -1204,7 +1204,7 @@ def PCA_plotting(wrk_dir,name):
                                comp_loc=f"scalers/comp_flux.bin")
     
     data = []
-    for file in test_data.locations:
+    for file in tqdm.tqdm(test_data.locations):
         data.append(np.loadtxt(file).reshape(1, -1))
     D = np.concatenate(data,axis=0)
     D[D<1e-11] = 1e-11
