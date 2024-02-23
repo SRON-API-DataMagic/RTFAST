324cca5b1e16c9c36aae5880812a0d216cde6a8c
benr <b.ricketts@sron.nl>
2024-02-23 17:56:01 +0100

    Attempt with weighted loss

--- test.py
+++ test.py
@@ -123,7 +123,7 @@ model.float()
 optimizer = Adam(model.parameters(),lr = 0.001)
 
 loss_fn = PCALoss(val_dataset.pca.explained_variance_ratio_, device)
-loss_fn = nn.MSELoss()
+#loss_fn = nn.MSELoss()
 
 train = train_flux
 test =  test_flux
