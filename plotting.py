cb13ddbb2ce6c157f83038e170366a657305acc6
benr <b.ricketts@sron.nl>
2024-02-29 11:10:31 +0100

    Take mean along axis 1 for test loss

--- plotting.py
+++ plotting.py
@@ -1485,7 +1485,7 @@ def reconstruct_emulator(dataset,model):
 
 def loss_calc(data,model,variances):
     log_vars_ratios = np.log10(variances/np.min(variances))+1
-    loss = np.mean(((model-np.asarray(data))**2)*log_vars_ratios)
+    loss = np.mean(((model-np.asarray(data))**2)*log_vars_ratios,axis=1)
     return loss
 
 def main():
