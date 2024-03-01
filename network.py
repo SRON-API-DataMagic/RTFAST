ac0ef1693ae5b1f06e6dbd71e59de3c2cc6d238f
benr <b.ricketts@sron.nl>
2024-03-01 11:15:14 +0100

    Increase to neural network size

--- network.py
+++ network.py
@@ -279,6 +279,12 @@ class PCAFluxNetwork(nn.Module):
                                          nn.ReLU(),
                                          nn.Linear(512, 512),
                                          nn.ReLU(), 
+                                         nn.Linear(512, 512),
+                                         nn.ReLU(),
+                                         nn.Linear(512, 512),
+                                         nn.ReLU(),
+                                         nn.Linear(512, 512),
+                                         nn.ReLU(), 
                                          nn.Linear(512, output_len))
     
     def forward(self,pars):
