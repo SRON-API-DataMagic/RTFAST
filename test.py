1a1ec0f2960605a9430756fd97d0db0f1047000e
benr <b.ricketts@sron.nl>
2024-03-04 11:34:10 +0100

    Train for 4000 epochs

--- test.py
+++ test.py
@@ -159,7 +159,7 @@ test =  test_flux
 
 grid_training_loop(model, optimizer, train, test, train_dataloader, 
                    val_dataloader, loss_fn, device, "PCA", "flux", 
-                   epochs = 5000)
+                   epochs = 4000)
 
 """
 bottleneck_training_loop(model, optimizer,train_dataloader, 
