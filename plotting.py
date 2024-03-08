0573123de12b6a92c404a60b868d2214cb900593
benr <b.ricketts@sron.nl>
2024-03-08 14:13:58 +0100

    Fixing parallelisation again

--- plotting.py
+++ plotting.py
@@ -1534,7 +1534,7 @@ def main():
     print("Parallelized model generation")
 
     print("Generating flux models")
-    flux =  Parallel(n_jobs=20,verbose=5)(delayed(generator.rtdist_flux)(pars,egrid_lo,egrid_hi)
+    flux =  Parallel(n_jobs=20,verbose=5,backend="multiprocessing")(delayed(generator.rtdist_flux)(pars,egrid_lo,egrid_hi)
                                     for pars in theta_flux)
     flux = np.asarray(flux)
     print("Checking for spectra below threshold")
