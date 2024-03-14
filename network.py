3fff8798975bdf525b2a7c11a6e31a0f2710c894
benr <b.ricketts@sron.nl>
2024-03-14 13:47:24 +0100

    Reduced depth, increased width

--- network.py
+++ network.py
@@ -297,25 +297,17 @@ class PCANetwork(nn.Module):
     
     def __init__(self,num_pars,output_len):
         super().__init__()
-        self.LinearStack = nn.Sequential(nn.Linear(num_pars, 512),
+        self.LinearStack = nn.Sequential(nn.Linear(num_pars, 1024),
                                          nn.GELU(),
-                                         nn.Linear(512, 512),
+                                         nn.Linear(1024, 1024),
                                          nn.GELU(),
-                                         nn.Linear(512, 512),
+                                         nn.Linear(1024, 1024),
                                          nn.GELU(),
-                                         nn.Linear(512, 512),
+                                         nn.Linear(1024, 1024),
                                          nn.GELU(),
-                                         nn.Linear(512, 512),
+                                         nn.Linear(1024, 1024),
                                          nn.GELU(),
-                                         nn.Linear(512, 512),
-                                         nn.GELU(),
-                                         nn.Linear(512, 512),
-                                         nn.GELU(), 
-                                         nn.Linear(512, 512),
-                                         nn.GELU(),
-                                         nn.Linear(512, 512),
-                                         nn.GELU(), 
-                                         nn.Linear(512, output_len))
+                                         nn.Linear(1024, output_len))
     
     def forward(self,pars):
         return self.LinearStack(pars)
