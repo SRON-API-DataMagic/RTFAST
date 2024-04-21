740c643337a4a42b25b66dd6abfd7760b9d2fd7c
benr <b.ricketts@sron.nl>
2024-04-21 10:15:02 +0200

    Train after generating lags

--- test.py
+++ test.py
@@ -269,7 +269,7 @@ def main():
     generate_lags_from_parameters(egrid_lo,egrid_hi,ReIm=-1)
     generate_lags_from_parameters(egrid_lo,egrid_hi,ReIm=-2)
     """
-    exit()
+    
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     
     pars_list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,21,22,23]
