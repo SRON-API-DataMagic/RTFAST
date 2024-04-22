2f2d6a4c6010d0d175be835984ba6aeda6067929
benr <b.ricketts@sron.nl>
2024-04-22 10:07:17 +0200

    Remove redundant data

--- processing.py
+++ processing.py
@@ -130,8 +130,7 @@ def removeRedundantData():
     """
     locations = "./data/locations/"
     labels = ["locs_10_spectra_tra.csv","locs_10_spectra_val.csv",
-              "locs_20_spectra_tra.csv","locs_20_spectra_val.csv",
-              "loc_flux_test.csv"]
+              "locs_20_spectra_tra.csv","locs_20_spectra_val.csv"]
     
     spectra_names = []
     for fname in labels:
