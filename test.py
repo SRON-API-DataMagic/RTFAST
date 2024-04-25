a917f3ab3f1a8c8b2454c4bc3771d147f70350e8
benr <b.ricketts@sron.nl>
2024-04-25 13:11:57 +0200

    Run for 1000 epochs at maximum

--- test.py
+++ test.py
@@ -303,7 +303,7 @@ def main():
     
     grid_training_loop(model, optimizer, train, test, tra_loader, 
                        val_loader, loss_fn, device, "20_pars", "flux", 
-                       epochs = 100)
+                       epochs = 1000)
     
 
 if __name__ == "__main__":
