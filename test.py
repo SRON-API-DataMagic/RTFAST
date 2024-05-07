cb9b2ae5cb9c951169bad6cc94525c910db8c2e3
benr <b.ricketts@sron.nl>
2024-05-07 09:23:56 +0200

    Try process with fewer workers

--- test.py
+++ test.py
@@ -78,7 +78,7 @@ def generate_lags_from_parameters(egrid_lo,egrid_hi,fmin,fmax,ReIm=-1):
     print(f"Loading tra in {no_loads_tra} sets")
     
     
-    with ThreadPoolExecutor() as executor:
+    with ProcessPoolExecutor(max_workers=20) as executor:
         for i in range(3,4): #generate 4e6 datapoints
             if i != no_loads_val-1:
                 pars_val = theta_val[i*load_size:(i+1)*load_size]
