4a27aaa5e3d5bb224d7b606a1d7e2c7cbb2c3948
Ben Ricketts <b.ricketts@sron.nl>
2024-05-17 11:12:16 +0200

    Typo

--- test.py
+++ test.py
@@ -75,7 +75,7 @@ def generate_lags_from_parameters(egrid_lo,egrid_hi,fmin,fmax,ReIm=-1,
     print(f"Loading val in {no_loads_val} sets")
     print(f"Loading tra in {no_loads_tra} sets")
     
-    with ProcessPoolExecutor(maxworkers=8) as executor:
+    with ProcessPoolExecutor(max_workers=8) as executor:
         for i in range(4): #generate 4e6 datapoints
             print(f"Generating load {i}")
             if i != no_loads_val-1:
