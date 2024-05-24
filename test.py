f707f886a866bb3eeb012dde64937c30772dd6c6
Ben Ricketts <b.ricketts@sron.nl>
2024-05-24 16:34:52 +0200

    Stop program after generating data

--- test.py
+++ test.py
@@ -234,6 +234,7 @@ def main():
                                       start=start)
         start = False
     
+    exit()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     
     pars_list = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,21,22,23]
