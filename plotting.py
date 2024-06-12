d2bc6c4ac351c0103ed36c69da97c932c5f22738
Ben Ricketts <b.ricketts@sron.nl>
2024-06-12 15:49:56 +0200

    More shades

--- plotting.py
+++ plotting.py
@@ -1480,10 +1480,13 @@ def PCA_plotting(wrk_dir,name):
     mean_res = np.mean(residuals_signed,axis=0)*100
     res_25 = np.quantile(residuals_signed,0.25,axis=0)*100
     res_75 = np.quantile(residuals_signed,0.75,axis=0)*100
-    res_95 = np.quantile(residuals_signed,0.99,axis=0)*100
-    res_05 = np.quantile(residuals_signed,0.01,axis=0)*100
+    res_95 = np.quantile(residuals_signed,0.95,axis=0)*100
+    res_05 = np.quantile(residuals_signed,0.05,axis=0)*100
+    res_99 = np.quantile(residuals_signed,0.99,axis=0)*100
+    res_01 = np.quantile(residuals_signed,0.01,axis=0)*100
     
-    plt.fill_between(emid, res_05, res_95,color="b",alpha=0.25,label="99%")
+    plt.fill_between(emid, res_01, res_99,color="b",alpha=0.2,label="99%")
+    plt.fill_between(emid, res_05, res_95,color="b",alpha=0.25,label="95%")
     plt.fill_between(emid, res_25, res_75,color="b",alpha=0.5,label="50%")
     plt.plot(emid,mean_res,c="b")
     plt.xlabel("Energy (keV)")
