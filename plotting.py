17b629c5464488bcfd314ff95f0618b167e7d9a6
Ben Ricketts <b.ricketts@sron.nl>
2024-07-04 09:17:44 +0200

    Plot correct model

--- plotting.py
+++ plotting.py
@@ -1205,7 +1205,7 @@ def main():
     wrk_dir = os.getcwd()
     set_envir_vars(wrk_dir)
     
-    PCA_plotting(wrk_dir,"active_best_flux")
+    PCA_plotting(wrk_dir,"20_pars_flux")
     
 if __name__ == "__main__":
     main()
