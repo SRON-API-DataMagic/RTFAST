b1f7c8127735bacc5343928a8f6bfde20ed93f6b
Ben Ricketts <b.ricketts@sron.nl>
2024-08-17 09:31:33 +0200

    fix

--- network.py
+++ network.py
@@ -56,8 +56,7 @@ class DynamicNetwork(nn.Module):
     
     def __init__(self,num_pars,output_len,num_layers,nodes,activation):
         super().__init__()
-        if activation == "GELU":
-            act_type = nn.GELU()
+        act_type = nn.GELU()
         modules = []
         #specify input stack
         modules.append(nn.Linear(num_pars, nodes))
