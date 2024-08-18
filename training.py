8bb7f10075bd86d3a8fcdb50f079d450141cf5a9
Ben Ricketts <b.ricketts@sron.nl>
2024-08-18 23:22:04 +0200

    Increase time before early stopping

--- training.py
+++ training.py
@@ -256,7 +256,7 @@ def grid_training_loop(model, optimizer, train, test, train_dataloader,
     loss_best = 20
     print("Beginning training")
     
-    while epoch < epochs and imp_flag < 50:
+    while epoch < epochs and imp_flag < 100:
         imp_flag += 1
         #time_st = time.time()
         print(f"Epoch {epoch+1} \n -----------------------")
