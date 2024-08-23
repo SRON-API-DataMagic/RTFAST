bcb3b12438d70dcb26288a917bd8fcded5af1e04
Ben Ricketts <b.ricketts@sron.nl>
2024-08-23 14:10:18 +0200

    Correction

--- plotting.py
+++ plotting.py
@@ -577,7 +577,7 @@ def loss_calc(data,model,variances):
 def main():
     wrk_dir = os.getcwd()
     num_models = 3
-    run_plot(wrk_dir,"ensemble_{num_models}",plot_pca=False,plot_loss=True,
+    run_plot(wrk_dir,f"ensemble_{num_models}",plot_pca=False,plot_loss=True,
              num_models=num_models)
     
 if __name__ == "__main__":
