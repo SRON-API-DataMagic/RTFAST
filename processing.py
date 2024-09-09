fe5348e35e83d53d365b40273ed12134ed0ab602
Ben Ricketts <b.ricketts@sron.nl>
2024-09-09 14:42:38 +0200

    Bug fix

--- processing.py
+++ processing.py
@@ -70,7 +70,7 @@ def saveData(dataset, pars, destination, fname, current_locs = None, lags = None
     #overwriting
     try:
         if lags == None:
-            files = glob.glob("/data/spectra/*.txt")
+            files = glob.glob("./data/spectra/*.txt")
             for i,file in enumerate(files):
                 tmp = file.replace("./data/spectra/spectra_","")
                 tmp = int(tmp.replace(".txt",""))
