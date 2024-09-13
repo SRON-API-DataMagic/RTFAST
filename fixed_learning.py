a659bbae672a04dbccbf1bb18a638f06a54c5b18
Ben Ricketts <b.ricketts@sron.nl>
2024-09-13 10:30:19 +0200

    Iterate over learning rates

--- fixed_learning.py
+++ fixed_learning.py
@@ -117,11 +117,13 @@ def main():
     train = train_flux
     test =  test_flux
     
+    learning_rates = [5e-4,1e-3,5e-3,1e-2]
+    
     for i in range(1,10):
         print(f"Training model {i+1}")
         model = DynamicNetwork(17,comps,12,256)
         model.to(device)
-        optimizer = Adam(model.parameters(), lr=5e-4)
+        optimizer = Adam(model.parameters(), lr=learning_rates[i-1])
         
         grid_training_loop(model, optimizer, train, test, tra_loader, 
                            val_loader, loss_fn, device, f"{i}_20_pars", "flux", 
