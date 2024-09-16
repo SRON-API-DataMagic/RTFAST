2255bc516372045b0b1b9130a65b9bb86b2f609b
Ben Ricketts <b.ricketts@sron.nl>
2024-09-16 09:31:20 +0200

    Slower learning rate

--- fixed_learning.py
+++ fixed_learning.py
@@ -124,7 +124,7 @@ def main():
         print(f"Training model {i+1}")
         model = DynamicNetwork(17,comps,blocks[i],nodes[i])
         model.to(device)
-        optimizer = Adam(model.parameters(), lr=1e-4)
+        optimizer = Adam(model.parameters(), lr=1e-5)
         
         grid_training_loop(model, optimizer, train, test, tra_loader, 
                            val_loader, loss_fn, device, f"{i}_20_pars", "flux", 
