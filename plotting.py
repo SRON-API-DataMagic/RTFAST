a6f9c34fa01f5336180b17bcee31a447b168fd3b
Ben Ricketts <b.ricketts@sron.nl>
2024-09-30 10:27:03 +0200

    Plot 6 models

--- plotting.py
+++ plotting.py
@@ -592,7 +592,7 @@ def main():
     
     new_set(range_AGN, pars_list, negatives, logged, egrid_lo, egrid_hi)
     """
-    num_models = 3
+    num_models = 6
     run_plot(wrk_dir,f"ensemble_{num_models}",plot_pca=False,plot_loss=True,
              plot_heatmaps=True,plot_samples=False,
              num_models=num_models)
