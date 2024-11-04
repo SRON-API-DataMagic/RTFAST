402ebc4b113dea22234b8265706d0e797a5531c2
Ben Ricketts <b.ricketts@sron.nl>
2024-11-04 16:25:18 +0000

    Fix

--- plotting.py
+++ plotting.py
@@ -452,7 +452,7 @@ def run_plot(wrk_dir,name,plot_pca = False,plot_loss = False,
     plt.savefig("loss/hist_frac_mean.png")
     plt.close()
     
-    plt.plot(np.cumsum(resids)/np.sum(resids), 10**sort_par)
+    plt.plot(10**sort_par,np.cumsum(resids)/np.sum(resids))
     plt.xlabel("Cornal height")
     plt.ylabel("Cumulative error")
     plt.savefig("loss/cum_frac_error.png")
