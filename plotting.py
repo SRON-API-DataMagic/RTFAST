793b76a7f72cfbf811af2a6d0ab9417d36ae5622
Ben Ricketts <b.ricketts@sron.nl>
2024-11-04 16:33:04 +0000

    Fix

--- plotting.py
+++ plotting.py
@@ -452,11 +452,12 @@ def run_plot(wrk_dir,name,plot_pca = False,plot_loss = False,
     plt.savefig("loss/hist_frac_mean.png")
     plt.close()
     
-    plt.plot(np.logspace(np.log10(1.5),2,1000),np.linspace(0,1,1000))
-    plt.plot(10**sort_par,np.cumsum(resids)/np.sum(resids))
+    plt.plot(np.logspace(np.log10(1.5),2,1000),np.linspace(0,1,1000),label = "exactly average error")
+    plt.plot(10**sort_par,np.cumsum(resids)/np.sum(resids),label="Test set")
     plt.xlabel("Cornal height")
     plt.ylabel("Cumulative error")
     plt.xscale("log")
+    plt.legend()
     plt.savefig("loss/cum_frac_error.png")
     plt.close()
     
