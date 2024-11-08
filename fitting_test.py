6d9d82cee6684d0fcf2a05d2882c280344f6d790
Ben Ricketts <b.ricketts@sron.nl>
2024-11-08 14:06:58 +0000

    Fix inclination and logNe to truth

--- fitting_test.py
+++ fitting_test.py
@@ -432,14 +432,14 @@ def insert_fixed_pars(theta):
     """
     #theta = np.insert(theta,0,nn_pars[0]) #fixed height
     #theta = np.insert(theta,1,nn_pars[1]) #fixed spin
-    #theta = np.insert(theta,2,nn_pars[2]) #fixed inclination
+    theta = np.insert(theta,2,nn_pars[2]) #fixed inclination
     #theta = np.insert(theta,3,nn_pars[3]) #fixed inner radius
     theta = np.insert(theta,4,nn_pars[4]) #fixed outer radius
     #theta = np.insert(theta,5,nn_pars[5]) #fixed z
     #theta = np.insert(theta,6,nn_pars[6]) #fixed gamma
     #theta = np.insert(theta,7,nn_pars[7]) #fixed distance
     #theta = np.insert(theta,8,nn_pars[8]) #fixed afe
-    #theta = np.insert(theta,9,nn_pars[9]) #fixed logNe
+    theta = np.insert(theta,9,nn_pars[9]) #fixed logNe
     theta = np.insert(theta,10,nn_pars[10]) #fixed kte
     #theta = np.insert(theta,11,nn_pars[11]) #fixed boost
     theta = np.insert(theta,12,nn_pars[12]) #fixed mass
@@ -482,13 +482,11 @@ def convolve_sim_fixed(theta):
     pred = resp.convolve_response(pred,"xspec")
     return pred
 
-nn_pars_indices_full = [0,1,2,3,5,6,7,8,9,11,16,17]
+nn_pars_indices_full = [0,1,3,5,6,7,8,11,16,17]
 nn_pars_true = np.asarray(nn_pars)[nn_pars_indices_full]
-labels_plots = np.delete(labels,[4,10,12,13,14,15])
+labels_plots = np.delete(labels,[2,4,9,10,12,13,14,15])
 
-ndim=12
-
-ndim, nwalkers = 12, 100
+ndim, nwalkers = len(nn_pars_indices_full), 100
 start_pos = (np.asarray(nn_pars)[nn_pars_indices_full][:,np.newaxis] + np.random.randn(ndim,nwalkers)*1e-5).T
 
 from multiprocessing import Pool
