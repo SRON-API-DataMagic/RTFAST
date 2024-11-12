dca5712e53852776e738570abffa2adc146237b4
Ben Ricketts <b.ricketts@sron.nl>
2024-11-12 13:50:54 +0100

    Bug fixes

--- fitting_test.py
+++ fitting_test.py
@@ -511,7 +511,7 @@ plt.close()
 
 ndim, nwalkers = len(nn_pars_indices_full), 100
 start_pos = (np.asarray(nn_pars)[nn_pars_indices_full][:,np.newaxis]
-             + (np.random.randn(ndim,nwalkers)*scale_perturbs.T))
+             + (np.random.randn(ndim,nwalkers)*scale_perturbs.T)).T
 
 from multiprocessing import Pool
 
@@ -520,8 +520,7 @@ backend = emcee.backends.HDFBackend(filename)
 backend.reset(nwalkers, ndim)
 
 with Pool() as pool:
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_likelihood_fixed, 
-                                    pool=pool,backend=backend)
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_likelihood_fixed,backend=backend)
     sampler.run_mcmc(start_pos, int(1e4), progress=True)
 
 print(
