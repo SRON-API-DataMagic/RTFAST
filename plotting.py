58dc3437d630f71d10ea5502f2b1d6a1ea90cdf1
Ben Ricketts <b.ricketts@sron.nl>
2024-11-29 15:58:01 +0100

    Replot all except pca

--- plotting.py
+++ plotting.py
@@ -589,7 +589,7 @@ def main():
     new_set(range_AGN, pars_list, negatives, logged, egrid_lo, egrid_hi)
     """
     num_models = 7
-    run_plot(wrk_dir,f"ensemble_{num_models}",plot_pca=True,plot_loss=True,
+    run_plot(wrk_dir,f"ensemble_{num_models}",plot_pca=False,plot_loss=True,
              plot_heatmaps=True,plot_samples=True,
              num_models=num_models)
     
