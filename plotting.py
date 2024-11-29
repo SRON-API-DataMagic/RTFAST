887143207dfab188ee5da5a608232a310d910070
Ben Ricketts <b.ricketts@sron.nl>
2024-11-29 16:30:13 +0100

    Redo cbars

--- plotting.py
+++ plotting.py
@@ -216,6 +216,9 @@ def PCA_plot(test_data,D,labels,name,pars_list,e_ticks):
         c_ticks = [10**(Z_center-1.5), 10**(Z_center-1), 10**(Z_center-0.5), 10**(Z_center),
                     10**(Z_center+0.5),10**(Z_center+1),10**(Z_center+1.5)]
         cbar = plt.colorbar(ticks=c_ticks, format='%.0e', norm=norm, extend='max')
+        cbar.ax.set_yticklabels([r"$10^{-3}$",r"$3 \times 10^{-3}$",
+                                 r"$10^{-2}$",r"$3 \times 10^{-2}$",
+                                 r"$10^{-1}$"])
         plt.yticks(ticks,labels=tick_labels)
         plt.xticks(np.arange(0,recon_resid.shape[1],recon_resid.shape[1]/5), 
                    labels=e_ticks)
@@ -470,6 +473,9 @@ def run_plot(wrk_dir,name,plot_pca = False,plot_loss = False,
             c_ticks = [10**(-3), 10**(-2.5), 10**(-2), 10**(-1.5),
                         10**(-1)]
             cbar = plt.colorbar(ticks=c_ticks, format='%.0e', norm=norm, extend='max')
+            cbar.ax.set_yticklabels([r"$10^{-3}$",r"$3 \times 10^{-3}$",
+                                     r"$10^{-2}$",r"$3 \times 10^{-2}$",
+                                     r"$10^{-1}$"])
             plt.yticks(ticks,labels=tick_labels)
             plt.xticks(np.arange(0,residuals.shape[1],residuals.shape[1]/5), 
                        labels=e_ticks)
