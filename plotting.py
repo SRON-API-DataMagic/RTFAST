c2667b637d90c45b853a73ef1941960d453fe15e
Ben Ricketts <b.ricketts@sron.nl>
2024-11-29 15:56:14 +0100

    Replot all

--- plotting.py
+++ plotting.py
@@ -589,8 +589,8 @@ def main():
     new_set(range_AGN, pars_list, negatives, logged, egrid_lo, egrid_hi)
     """
     num_models = 7
-    run_plot(wrk_dir,f"ensemble_{num_models}",plot_pca=False,plot_loss=False,
-             plot_heatmaps=False,plot_samples=False,
+    run_plot(wrk_dir,f"ensemble_{num_models}",plot_pca=True,plot_loss=True,
+             plot_heatmaps=True,plot_samples=True,
              num_models=num_models)
     
 if __name__ == "__main__":
